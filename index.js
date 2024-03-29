4fb04a9427921be84885b816addcac192b42eee8
meshackmesto <meshackorina93@gmail.com>
2024-03-29 09:32:16 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let spy;
+( () => {
+    spy = sinon.spy( console, 'log' );
+  } );
+  function writeCards( namesArray, event ) {
+    let cards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      cards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return cards
+  }
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
